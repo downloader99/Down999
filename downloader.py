ecf31679c9420094ff6412a50c08563d4f79c789
downloader99 <akashshigvan108@gmail.com>
2025-02-04 14:14:28 +0530

    Fixed exception handling and added new download files

--- downloader.py
+++ downloader.py
@@ -2,12 +2,13 @@ import yt_dlp
 
 def download_video(url):
     ydl_opts = {
-        'outtmpl': './downloads/%(title)s.%(ext)s',  # Saves videos to a folder called 'downloads'
+        'format': 'best',
+        'outtmpl': '%(title)s.%(ext)s',
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
 if __name__ == "__main__":
-    video_url = input("Enter the xHamster video URL: ")
+    video_url = input("Enter the video URL: ")
     download_video(video_url)
